92335b28c4ac7d75108e4a2349b242d7383d36ec
Daniel Ördög <odan9imp@gmail.com>
2022-05-20 11:41:30 +0300

    player changes and other stuff, pretty much core mechanic is working

--- Assets/Daniel/Player/Script/FillArea.cs
+++ Assets/Daniel/Player/Script/FillArea.cs
@@ -13,12 +13,19 @@ public class FillArea : MonoBehaviour
     float minPercent = 0f;
     float maxPercent = 100f;
     public TextMeshPro text;
+    public GameObject RefillUI;
+    public GameObject boxIsntRightText;
+    public GameObject refillingText;
+
+
     
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         productPercent = 100f;
+        boxIsntRightText.SetActive(false);
+        refillingText.SetActive(false);
     }
 
     void Update()
@@ -30,27 +37,62 @@ public class FillArea : MonoBehaviour
         else if (productPercent >= maxPercent)
             productPercent = 100f;
 
-        text.text = (productPercent.ToString());
+        text.text = ($"{Mathf.Round(productPercent).ToString()} %");
+        if (productPercent <= 50)
+            text.color = Color.red;
+        else  
+            text.color = Color.black;
+
+
 
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(typeRequired))
+        {
             StartCoroutine(FillUp());
+            StartCoroutine(RefillingText());
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
         else 
-            Debug.Log("The products in this box don't belong to this shelf");
+           StartCoroutine(NotRightBox());
     }
 
     IEnumerator FillUp()
     {
         mainCam.GetComponent<CameraMovement>().enabled = false;
         player.GetComponent<PlayerMovement>().enabled = false;
+
         yield return new WaitForSeconds(4f);
         productPercent += 25f;
+        Destroy(GameObject.FindGameObjectWithTag("dairyBox"));
+        Destroy(GameObject.FindGameObjectWithTag("houseHoldBox"));
+        Destroy(GameObject.FindGameObjectWithTag("vegAndFruitBox"));
+        Destroy(GameObject.FindGameObjectWithTag("petFoodBox"));
+        Destroy(GameObject.FindGameObjectWithTag("drinkBox"));
+        Destroy(GameObject.FindGameObjectWithTag("paperBox"));
+        Destroy(GameObject.FindGameObjectWithTag("meatBox"));
         mainCam.GetComponent<CameraMovement>().enabled = true;
         player.GetComponent<PlayerMovement>().enabled = true;
+        RefillUI.GetComponent<GrabBoxes>().hasBox = false;
+    }
 
+    IEnumerator RefillingText()
+    {
+        refillingText.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        refillingText.SetActive(false);
+    }
+
+    IEnumerator NotRightBox()
+    {
+        boxIsntRightText.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        boxIsntRightText.SetActive(false);
 
     }
 }
